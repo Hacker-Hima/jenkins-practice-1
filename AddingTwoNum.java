34aa68a642a37a022f803896ef99ded731c74ca8
Hacker-Hima <himapc2007@gmail.com>
2026-07-29 16:21:53 +0530

    Refining

--- AddingTwoNum.java
+++ AddingTwoNum.java
@@ -1,12 +1,9 @@
 
-import java.util.*;
 public class AddingTwoNum{
   public static void main(String[] args){
-    Scanner sc=new Scanner(System.in);
-    int a=sc.nextInt();
-    int b=sc.nextInt();
+    int a=10;
+    int b=20;
     int res=a+b;
     System.out.print("result"+res);
-    sc.close();
   }
 }
